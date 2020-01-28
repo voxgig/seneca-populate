dda4cc20ac4460c404000952f57a82ff6bb9d97f
rjrodger <richard@ricebridge.com>
2020-01-28 20:34:33 +0000

    add docs file

---
+++ populate-docs.js
@@ -0,0 +1,14 @@
+const Joi = require('@hapi/joi')
+
+module.exports = {
+  cmd_import: {
+    desc: 'Import a JSON data file (using seneca-mem-store dump format).'
+  },
+  cmd_export: {
+    desc: 'Export a JSON data file (using seneca-mem-store dump format).'
+  },
+  cmd_populate: {
+    desc:
+      'Import JSON data (using seneca-mem-store) and run data construction messages (using seneca-msg-test).'
+  }
+}
